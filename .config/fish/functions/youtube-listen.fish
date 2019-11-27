822751b5e1851384aa968cf2980abfd309b29bdb
Yorick Peterse <yorick@yorickpeterse.com>
2019-11-27 21:07:52 +0100

    Removed --cache from youtube-listen

--- .config/fish/functions/youtube-listen.fish
+++ .config/fish/functions/youtube-listen.fish
@@ -1,4 +1,4 @@
 function youtube-listen -d 'Streams a YouTube video with only audio'
-    mpv $argv[1] --no-video --volume=100 --cache=4096 --quiet 2>&1 >/dev/null
+    mpv $argv[1] --no-video --volume=100 --quiet 2>&1 >/dev/null
 end
 
