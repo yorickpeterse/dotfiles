dec4fe72d7046791139e5fe608a8e4a0a98254ae
Yorick Peterse <yorick@yorickpeterse.com>
2020-11-14 16:42:13 +0100

    Tweak the highlight color of ripgrep

--- dotfiles/.config/fish/functions/rg.fish
+++ dotfiles/.config/fish/functions/rg.fish
@@ -1,9 +1,8 @@
 function rg -d 'Runs ripgrep with custom colors'
     command rg \
         --colors 'match:none' \
-        --colors 'match:fg:yellow' \
-        --colors 'match:style:bold' \
-        --colors 'match:style:underline' \
+        --colors 'match:fg:black' \
+        --colors 'match:bg:242,222,145' \
         --colors 'path:none' \
         --colors 'path:style:bold' \
         --colors 'column:none' \
