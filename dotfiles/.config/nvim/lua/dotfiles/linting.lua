9a3c79ee59e052fe214ac36f33b39ce995872851
Yorick Peterse <yorick@yorickpeterse.com>
2021-05-14 23:23:37 +0200

    Update linter configuration

--- dotfiles/.config/nvim/lua/dotfiles/linting.lua
+++ dotfiles/.config/nvim/lua/dotfiles/linting.lua
@@ -1,14 +1,12 @@
 -- Configuration for nvim-lint (https://github.com/mfussenegger/nvim-lint)
 
 local lint = require('lint')
-local lsp_severities = vim.lsp.protocol.DiagnosticSeverity
 
-lint.linters_by_ft = {
-  markdown = { 'vale' },
+lint.linters_by_ft = vim.tbl_extend('force', lint.linters_by_ft, {
+  -- gitcommit = { 'gitlint' },
   python = { 'flake8' },
-  ruby = { 'ruby' },
+  ruby = { 'ruby', 'rubocop' },
   sh = { 'shellcheck' },
-  text = {},
-}
+})
 
 -- vim: set foldmethod=marker
