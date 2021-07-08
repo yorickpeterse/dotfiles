1e7ae5d55b1d7c93501c06fa8f66a84b4fb41cac
Yorick Peterse <yorick@yorickpeterse.com>
2021-07-08 17:34:07 +0200

    Tweak characters for nvim-window

--- dotfiles/.config/nvim/lua/dotfiles/window.lua
+++ dotfiles/.config/nvim/lua/dotfiles/window.lua
@@ -1,4 +1,27 @@
+local chars = {}
+
+if vim.fn.hostname() == 'sharkie' then
+  chars = {
+    'a', 'r', 's', 't', 'g',
+    'k', 'n', 'e', 'i', 'o',
+    'w', 'f', 'p', 'l', 'u',
+    'y', 'c', 'd', 'v', 'm',
+    'h', 'j', 'b', 'q', 'x',
+    'z'
+  }
+else
+  chars = {
+    'a', 's', 'd', 'f', 'g',
+    'h', 'j', 'k', 'l', 'q',
+    'w', 'e', 'r', 't', 'y',
+    'u', 'i', 'o', 'p', 'z',
+    'x', 'c', 'v', 'b', 'n',
+    'm'
+  }
+end
+
 require('nvim-window').setup({
   border = 'none',
-  normal_hl = 'BlackOnLightYellow'
+  normal_hl = 'BlackOnLightYellow',
+  chars = chars,
 })
