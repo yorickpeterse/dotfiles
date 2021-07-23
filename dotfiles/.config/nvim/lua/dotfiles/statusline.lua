facb822f23bbbdfc04858f7a063eb5ac94d68a60
Yorick Peterse <yorick@yorickpeterse.com>
2021-07-23 19:44:47 +0200

    Fix statuslines for quickfix windows

--- dotfiles/.config/nvim/lua/dotfiles/statusline.lua
+++ dotfiles/.config/nvim/lua/dotfiles/statusline.lua
@@ -27,32 +27,19 @@ local function highlight(text, group)
   return '%#' .. group .. '#' .. text .. '%*'
 end
 
--- Renders the statusline for a quickfix window
-local function render_quickfix()
-  local window = vim.g.statusline_winid
-  local active = window == api.nvim_get_current_win()
-  local has_title, title =
-    pcall(api.nvim_win_get_var, window, 'quickfix_title')
-
-  return table.concat({
-    active and highlight(' %t ', active_hl) or ' %t ',
-    has_title and ' ' .. title or ''
-  })
-end
-
 -- Renders the status line.
 function M.render()
-  if vim.bo.ft == 'qf' then
-    return render_quickfix()
-  end
-
   local window = vim.g.statusline_winid
   local active = window == api.nvim_get_current_win()
   local buffer = api.nvim_win_get_buf(window)
   local name = ' ' .. icons.icon(vim.fn.bufname(buffer)) .. '%f '
+  local has_qf_title, qf_title =
+    pcall(api.nvim_win_get_var, window, 'quickfix_title')
 
   return table.concat({
     active and highlight(name, active_hl) or name,
+    has_qf_title and ' ' .. qf_title or '',
+    ' ',
     preview,
     modified,
     readonly,
