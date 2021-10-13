b582f8c4283d6dccc68ca3e0ec273ae8de564dcc
Yorick Peterse <git@yorickpeterse.com>
2021-10-13 22:32:45 +0200

    Fix unloading empty anonymous buffers, again

--- dotfiles/.config/nvim/lua/dotfiles/hooks.lua
+++ dotfiles/.config/nvim/lua/dotfiles/hooks.lua
@@ -73,9 +73,9 @@ end
 -- Deletes empty anonymous buffers when hiding them, so they don't pile up.
 function M.hide_buffer()
   local buffer = fn.bufnr()
-  local lines = api.nvim_buf_line_count(buffer)
+  local lines = fn.getbufline(buffer, 1, 1)
 
-  if fn.bufname(buffer) == '' and lines == 0 then
+  if fn.bufname(buffer) == '' and #lines[1] == 0 then
     -- The buffer is still in use at this point, so we must schedule the removal
     -- until after the hook finishes.
     vim.schedule(function()
