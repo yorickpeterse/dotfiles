5575b95250951e4ff1f96c5be1e2e834600f3ce9
Yorick Peterse <git@yorickpeterse.com>
2021-10-18 02:16:13 +0200

    Another trial with enabling Rust TreeSitter

--- dotfiles/.config/nvim/lua/dotfiles/treesitter.lua
+++ dotfiles/.config/nvim/lua/dotfiles/treesitter.lua
@@ -1,7 +1,7 @@
 require('nvim-treesitter.configs').setup {
   highlight = {
     enable = true,
-    disable = { 'ruby', 'rust' },
+    disable = { 'ruby' },
     additional_vim_regex_highlighting = false,
   },
 }
