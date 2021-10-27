df6263c85936379e12eb8b1eec45dec7cf7dcc1f
Yorick Peterse <git@yorickpeterse.com>
2021-10-28 00:04:33 +0200

    Always show LSP clients and in a muted colour

--- dotfiles/.config/nvim/lua/dotfiles/tabline.lua
+++ dotfiles/.config/nvim/lua/dotfiles/tabline.lua
@@ -10,7 +10,7 @@ local active_tab = 'TabLineSel'
 local inactive_tab = 'TabLine'
 local default_name = '[No Name]'
 local separator = '%='
-local lsp_hl = 'PmenuSel'
+local lsp_hl = 'TabLine'
 local lsp_icons = {
   clangd = icons.by_name('c'),
   gopls = icons.by_name('go'),
@@ -20,28 +20,43 @@ local lsp_icons = {
 
 local function lsp_status()
   local statuses = {}
-  local handled = {}
 
   for _, msg in ipairs(lsp.util.get_progress_messages()) do
-    if not msg.done and not handled[msg.name] then
-      local icon = lsp_icons[msg.name] or ''
-      local status =
-        highlight(icon .. ' ' .. msg.name .. ': ', lsp_hl) .. msg.title
+    if not msg.done and not statuses[msg.name] then
+      local status = msg.title
 
       if msg.percentage then
         status = status .. ' ' .. msg.percentage .. '%%'
       end
 
-      table.insert(statuses, status)
-      handled[msg.name] = true
+      statuses[msg.name] = status
     end
   end
 
-  if #statuses == 0 then
-    return ''
-  else
-    return forced_space .. table.concat(statuses, ' ') .. forced_space
+  for _, client in ipairs(lsp.get_active_clients()) do
+    if not statuses[client.name] then
+      statuses[client.name] = 'idle'
+    end
   end
+
+  if vim.tbl_isempty(statuses) then
+    return ''
+  end
+
+  local cells = {}
+
+  for name, status in pairs(statuses) do
+    local icon = lsp_icons[name]
+    local text = name .. ': ' .. status
+
+    if icon then
+      text = icon .. ' ' .. text
+    end
+
+    table.insert(cells, highlight(text, lsp_hl))
+  end
+
+  return forced_space .. table.concat(cells, ' ') .. forced_space
 end
 
 function M.render()
