4d461c55abf6e28c1c3c1484dc132396cf44e628
Yorick Peterse <git@yorickpeterse.com>
2021-12-14 02:03:42 +0100

    Add laptop support to the brightness function

--- dotfiles/.config/fish/functions/brightness.fish
+++ dotfiles/.config/fish/functions/brightness.fish
@@ -1,3 +1,10 @@
 function brightness
-    echo $argv | tee /sys/class/backlight/ddcci9/brightness
+    if test -d /sys/class/backlight/intel_backlight
+        set max (cat /sys/class/backlight/intel_backlight/max_brightness)
+        set new_value (math "round($argv * ($max / 100))")
+
+        echo $new_value | tee /sys/class/backlight/intel_backlight/brightness
+    else
+        echo $argv | tee /sys/class/backlight/ddcci9/brightness
+    end
 end
