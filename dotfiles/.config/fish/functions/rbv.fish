f16778700fd41d8fdf29d70e4e3d0f9c1b1aae38
Yorick Peterse <git@yorickpeterse.com>
2022-01-16 16:10:28 +0100

    Check if the rbv version exists

--- dotfiles/.config/fish/functions/rbv.fish
+++ dotfiles/.config/fish/functions/rbv.fish
@@ -2,6 +2,13 @@
 function rbv
     if set -q argv[1]
         set -g -x RBV_VERSION $argv[1]
+        set ruby_root "$HOME/.rubies/$RBV_VERSION"
+
+        if not test -d $ruby_root
+            echo "The version $RBV_VERSION is invalid"
+            return 1
+        end
+
         set -g -x RUBY_ROOT "$HOME/.rubies/$RBV_VERSION"
 
         fish_add_path -g "$RUBY_ROOT/bin"
