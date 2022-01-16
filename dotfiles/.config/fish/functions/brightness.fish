3cb6582103ed9a5110953db1f61bced19152d335
Yorick Peterse <git@yorickpeterse.com>
2022-01-16 02:20:56 +0100

    Fix brightness to use argv correctly

--- dotfiles/.config/fish/functions/brightness.fish
+++ dotfiles/.config/fish/functions/brightness.fish
@@ -1,10 +1,10 @@
 function brightness
     if test -d /sys/class/backlight/intel_backlight
         set max (cat /sys/class/backlight/intel_backlight/max_brightness)
-        set new_value (math "round($argv * ($max / 100))")
+        set new_value (math "round($argv[1] * ($max / 100))")
 
         echo $new_value | tee /sys/class/backlight/intel_backlight/brightness
     else
-        echo $argv | tee /sys/class/backlight/ddcci9/brightness
+        echo $argv[1] | tee /sys/class/backlight/ddcci9/brightness
     end
 end
