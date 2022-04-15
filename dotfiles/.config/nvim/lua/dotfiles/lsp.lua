8750228be03fc29bdd85e518f1c2966d8973d12f
Yorick Peterse <git@yorickpeterse.com>
2022-04-15 19:28:52 +0200

    Disable Lua LSP formatting
    This way formatting is always done using Stylua.

--- dotfiles/.config/nvim/lua/dotfiles/lsp.lua
+++ dotfiles/.config/nvim/lua/dotfiles/lsp.lua
@@ -124,7 +124,12 @@ do
   table.insert(rpath, 'lua/?/init.lua')
 
   config.sumneko_lua.setup({
-    on_attach = on_attach,
+    on_attach = function(client, bufnr)
+      client.resolved_capabilities.document_formatting = false
+      client.resolved_capabilities.document_range_formatting = false
+
+      on_attach(client, bufnr)
+    end,
     capabilities = capabilities,
     flags = flags,
     cmd = {
