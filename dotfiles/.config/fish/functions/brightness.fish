12590703a379c3cccc204e5044800333f09500f7
Yorick Peterse <git@yorickpeterse.com>
2023-01-06 15:35:42 +0100

    Revert "Handle my new desktop display's brightness"
    This reverts commit cd965645cf4274fdbe88210c74870d438911714d.

--- dotfiles/.config/fish/functions/brightness.fish
+++ dotfiles/.config/fish/functions/brightness.fish
@@ -5,8 +5,6 @@ function brightness
 
         echo $new_value | tee /sys/class/backlight/intel_backlight/brightness
     else
-        # My desktop display uses luminance values instead of brightness
-        # percentages, in a range of 0 to 250 (inclusive).
-        ddcutil setvcp 0x10 (math --scale 0 "$argv[1] * 2.5")
+        ddcutil setvcp 0x10 $argv[1]
     end
 end
