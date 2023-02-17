c4cf23ef9a4f5671c9d026320ce1bb1a5958cdd1
Yorick Peterse <git@yorickpeterse.com>
2023-02-17 20:22:31 +0100

    Disable Rust Treesitter
    The highlighting isn't as nice as the Vim-based one.

--- dotfiles/.config/nvim/lua/dotfiles/treesitter.lua
+++ dotfiles/.config/nvim/lua/dotfiles/treesitter.lua
@@ -16,7 +16,7 @@ require('nvim-treesitter.configs').setup({
   sync_install = false,
   highlight = {
     enable = true,
-    disable = { 'ruby' },
+    disable = { 'ruby', 'rust' },
     additional_vim_regex_highlighting = false,
   },
 })
