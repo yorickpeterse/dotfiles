718e49c936d73c6b74c52b57b26ddfd9b6870296
Yorick Peterse <git@yorickpeterse.com>
2023-04-18 20:55:34 +0200

    Add scripts for upgrading Fedora containers

--- Makefile
+++ Makefile
@@ -1,4 +1,11 @@
-all:
+dotfiles:
 	stow dotfiles -t ~/
 
-.PHONY: all
+fedora:
+	@mkdir -p ${PWD}/homes/fedora
+	@distrobox enter fedora -- fish ${PWD}/fedora/backup.fish > ${PWD}/seed.fish
+	@distrobox create --image fedora:38 --name fedora --home ${PWD}/homes/fedora --no-entry
+	@distrobox enter fedora -- fish ${PWD}/seed.fish
+	@rm ${PWD}/seed.fish
+
+.PHONY: dotfiles fedora
---
+++ fedora/backup.fish
@@ -0,0 +1,15 @@
+#!/usr/bin/env fish
+
+set packages (sudo dnf repoquery --qf '%{name}' --userinstalled)
+set coprs (sudo dnf copr list)
+
+echo "echo 'Configuring dnf...'"
+echo "echo 'max_parallel_downloads=10' | sudo tee --append /etc/dnf/dnf.conf >/dev/null"
+
+for line in $coprs
+    echo "echo 'Enabling $line...'"
+    echo "sudo dnf copr enable --assumeyes $line >/dev/null 2>&1"
+end
+
+echo "echo 'Installing packages...'"
+echo "sudo dnf install --assumeyes --quiet $packages"
