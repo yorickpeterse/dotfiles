c9d973d89661c18bdee268f74b08cf5c022612fd
Yorick Peterse <git@yorickpeterse.com>
2023-08-22 14:35:58 +0200

    Disable Treesitter for C
    When editing certain files, such as my QMK keymap, the C treesitter
    parser causes nvim to start consuming memory like crazy, eventually
    locking up everything (unless killed fast enough).

--- dotfiles/.config/nvim/lua/dotfiles/treesitter.lua
+++ dotfiles/.config/nvim/lua/dotfiles/treesitter.lua
@@ -16,7 +16,7 @@ require('nvim-treesitter.configs').setup({
   sync_install = false,
   highlight = {
     enable = true,
-    disable = { 'ruby', 'rust' },
+    disable = { 'ruby', 'rust', 'c' },
     additional_vim_regex_highlighting = false,
   },
 })
