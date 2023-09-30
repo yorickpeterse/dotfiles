247359e695ea5c6a470d18405b715c1110dd98e1
Yorick Peterse <git@yorickpeterse.com>
2023-09-30 04:13:17 +0200

    Drop the dag/vim-fish dependency
    The Treesitter parser for Fish is more than capable enough.

--- dotfiles/.config/nvim/lua/dotfiles/packages.lua
+++ dotfiles/.config/nvim/lua/dotfiles/packages.lua
@@ -4,7 +4,6 @@ local use = pkg.use
 use('nvim-lua/plenary.nvim')
 use('numToStr/Comment.nvim')
 use('YorickPeterse/rust.vim')
-use('dag/vim-fish')
 use('Vimjas/vim-python-pep8-indent')
 use('neovim/nvim-lspconfig')
 use('dcampos/nvim-snippy')
