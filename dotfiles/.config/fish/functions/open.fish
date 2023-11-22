06d21bc54fdeb2d8756f2e5e7193d504828a0e24
Yorick Peterse <git@yorickpeterse.com>
2023-11-22 19:51:55 +0100

    Fix open function for container paths

--- dotfiles/.config/fish/functions/open.fish
+++ dotfiles/.config/fish/functions/open.fish
@@ -1,3 +1,22 @@
 function open -d 'Opens a given file or directory'
-    xdg-open $argv
+    if set -q argv[1]
+        set path (realpath $argv[1])
+    else
+        echo 'You must specify a path to open'
+        return 1
+    end
+
+    # This ensures that when the command is run from a container, these mounted
+    # directories are openend using the correct host path. This would be a lot
+    # easier if we could just "overlay" the container's $HOME on top of the
+    # host's $HOME.
+    if test -n "$CONTAINER_ID"
+        set path (string replace --regex \
+            "/var/home/$USER/homes/$CONTAINER_ID/(Projects|Downloads)" \
+            "/var/home/$USER/\$1" $path)
+
+        /usr/bin/distrobox-host-exec xdg-open $path
+    else
+        /usr/bin/xdg-open $path
+    end
 end
