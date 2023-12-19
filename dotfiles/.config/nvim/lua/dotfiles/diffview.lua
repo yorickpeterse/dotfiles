8ba3df2e5c742766b39f96c0937a64af69c8b914
Yorick Peterse <git@yorickpeterse.com>
2023-12-19 19:32:00 +0100

    Fix cursor position for diffview
    This ensures that when jumping between files, the cursor is always
    placed at the top of the window instead of a semi random place.

--- dotfiles/.config/nvim/lua/dotfiles/diffview.lua
+++ dotfiles/.config/nvim/lua/dotfiles/diffview.lua
@@ -1,6 +1,13 @@
 local api = vim.api
 local actions = require('diffview.actions')
 
+local function top(func)
+  return function()
+    func()
+    vim.cmd.normal('gg')
+  end
+end
+
 require('diffview').setup({
   diff_binaries = false,
   enhanced_diff_hl = true,
@@ -19,13 +26,13 @@ require('diffview').setup({
     file_panel = {
       { 'n', 'j', actions.next_entry },
       { 'n', 'k', actions.prev_entry },
-      { 'n', '<CR>', actions.select_entry },
+      { 'n', '<CR>', top(actions.focus_entry) },
       { 'n', '-', actions.toggle_stage_entry },
       { 'n', 'U', actions.unstage_all },
       { 'n', 'X', actions.restore_entry },
       { 'n', 'R', actions.refresh_files },
-      { 'n', ']f', actions.select_next_entry },
-      { 'n', '[f', actions.select_prev_entry },
+      { 'n', ']f', top(actions.select_next_entry) },
+      { 'n', '[f', top(actions.select_prev_entry) },
     },
     file_history_panel = {
       { 'n', '<CR>', actions.select_entry },
