18f1cc90890829c5219e778ab359f36fca6979b4
Yorick Peterse <git@yorickpeterse.com>
2023-12-26 18:01:47 +0100

    Fix winbar for terminal buffers

--- dotfiles/.config/nvim/lua/dotfiles/winbar.lua
+++ dotfiles/.config/nvim/lua/dotfiles/winbar.lua
@@ -8,17 +8,25 @@ function M.render()
   local win = vim.g.statusline_winid
   local buf = api.nvim_win_get_buf(win)
   local typ = vim.bo[buf].buftype
-  local name = nil
-  local flags = nil
+  local name = ''
+  local flags = ''
 
   if typ == 'quickfix' then
     local has_title, qf_title =
       pcall(api.nvim_win_get_var, win, 'quickfix_title')
 
     name = (has_title and qf_title) and qf_title or NO_NAME
-    flags = ''
+  elseif typ == 'terminal' then
+    local bufname = fn.bufname(buf)
+    local parts = vim.split(bufname, ':', { trimempty = true })
+
+    if #parts == 3 then
+      bufname = 'term:' .. parts[3]
+    end
+
+    name = bufname
   else
-    local bufname = api.nvim_buf_get_name(buf)
+    local bufname = fn.bufname(buf)
 
     if bufname == '' then
       bufname = NO_NAME
@@ -29,17 +37,7 @@ function M.render()
       bufname = bufname:gsub('%%', '%%%%')
     end
 
-    if typ == 'terminal' and vim.startswith(bufname, 'term://') then
-      local parts = vim.split(bufname, ':', { trimempty = true })
-
-      if #parts == 3 then
-        bufname = 'term:' .. parts[3]
-      end
-    else
-      bufname = fn.fnamemodify(bufname, ':.')
-    end
-
-    name = bufname
+    name = fn.fnamemodify(bufname, ':.')
     flags = '%( %m%r%)'
   end
 
