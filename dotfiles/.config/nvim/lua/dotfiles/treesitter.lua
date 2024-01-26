62cbb25d865ba6c133af2c32bf1df8808e704b58
Yorick Peterse <git@yorickpeterse.com>
2024-01-26 17:27:56 +0100

    Disable Markdown Treesitter parsers
    These introduce too much latency in even slightly complicated documents.

--- dotfiles/.config/nvim/lua/dotfiles/treesitter.lua
+++ dotfiles/.config/nvim/lua/dotfiles/treesitter.lua
@@ -19,7 +19,7 @@ require('nvim-treesitter.configs').setup({
   sync_install = false,
   highlight = {
     enable = true,
-    disable = { 'ruby', 'rust', 'c' },
+    disable = { 'ruby', 'rust', 'c', 'markdown', 'markdown_inline' },
     additional_vim_regex_highlighting = false,
   },
   indent = {
