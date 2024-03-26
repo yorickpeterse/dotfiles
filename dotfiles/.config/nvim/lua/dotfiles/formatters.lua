717d28989eab8c87daf12ad37f2070317141b827
Yorick Peterse <git@yorickpeterse.com>
2024-03-26 15:50:08 +0100

    Add Inko formatters

--- dotfiles/.config/nvim/lua/dotfiles/formatters.lua
+++ dotfiles/.config/nvim/lua/dotfiles/formatters.lua
@@ -1,8 +1,19 @@
 require('conform').setup({
+  formatters = {
+    inko = {
+      meta = {
+        url = 'https://inko-lang.org/',
+        description = 'Format Inko source code according to the Inko style guide',
+      },
+      command = 'inko',
+      args = { 'fmt', '-' },
+    },
+  },
   formatters_by_ft = {
     fish = { 'fish_indent' },
     lua = { 'stylua' },
     python = { 'black' },
     zig = { 'zigfmt' },
+    inko = { 'inko' },
   },
 })
