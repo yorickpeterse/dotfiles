2a9fa716eaa6511fdcef5c266c6f8945628242a1
Yorick Peterse <git@yorickpeterse.com>
2024-03-29 15:35:48 +0100

    Move Inko formatter to conform.nvim

--- dotfiles/.config/nvim/lua/dotfiles/formatters.lua
+++ dotfiles/.config/nvim/lua/dotfiles/formatters.lua
@@ -1,14 +1,4 @@
 require('conform').setup({
-  formatters = {
-    inko = {
-      meta = {
-        url = 'https://inko-lang.org/',
-        description = 'Format Inko source code according to the Inko style guide',
-      },
-      command = 'inko',
-      args = { 'fmt', '-' },
-    },
-  },
   formatters_by_ft = {
     fish = { 'fish_indent' },
     lua = { 'stylua' },
