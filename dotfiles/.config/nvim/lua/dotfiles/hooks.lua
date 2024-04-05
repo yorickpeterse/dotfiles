3d997202aebd4b1e1364ffb98f258eaf12d107d8
Yorick Peterse <git@yorickpeterse.com>
2024-04-05 23:11:46 +0200

    Clear statuscolumn for terminals

--- dotfiles/.config/nvim/lua/dotfiles/hooks.lua
+++ dotfiles/.config/nvim/lua/dotfiles/hooks.lua
@@ -250,6 +250,7 @@ au('terminal', {
       vim.opt_local.number = false
       vim.opt_local.relativenumber = false
       vim.opt_local.signcolumn = 'no'
+      vim.opt_local.statuscolumn = ''
     end,
   },
 })
