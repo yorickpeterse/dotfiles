d1e498325ec1a4cb122d3ff61aa76ec9f72f250a
Yorick Peterse <git@yorickpeterse.com>
2024-05-23 01:10:37 +0200

    Install rustup toolchain for Inko

--- containers/install.fish
+++ containers/install.fish
@@ -14,6 +14,9 @@ run pip install --quiet $pip
 section 'Installing rustup'
 run rustup-init --quiet -y --no-modify-path
 
+section 'Installing Rust toolchain for Inko'
+run rustup toolchain install 1.70.0 -c clippy -c rustfmt -c rust-analyzer
+
 section 'Configuring Ruby'
 echo 'gem: --no-document --user --bindir ~/.local/share/gem/ruby/bin' >~/.gemrc
 run gem install --silent pry pry-doc pry-theme
