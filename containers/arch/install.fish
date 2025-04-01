e28af074536a6b821d2f5cbff64c192853e9013d
Yorick Peterse <yorick@yorickpeterse.com>
2025-04-01 03:01:31 +0200

    Don't install toolchain for Inko
    This is handled by the project itself already.

--- containers/arch/install.fish
+++ containers/arch/install.fish
@@ -4,13 +4,11 @@ source ../helpers.fish
 
 set completions /etc/fish/completions
 
-section 'Installing Rust toolchain for Inko'
-run rustup toolchain install 1.70.0 -c clippy -c rustfmt -c rust-analyzer
-
 source ~/.config/fish/config.fish
 
 section 'Configuring Rust'
 run rustup install stable
+run rustup default stable
 run rustup component add rust-src rust-analyzer clippy rustfmt
 cp cargo-config.toml ~/.cargo/config.toml
 
