a5c09d5659fa40dbe65ca1d52809b831bc1c4a90
Yorick Peterse <yorick@yorickpeterse.com>
2025-04-10 16:01:26 +0200

    Remove perf.data after running prof

--- dotfiles/.config/fish/functions/prof.fish
+++ dotfiles/.config/fish/functions/prof.fish
@@ -1,4 +1,5 @@
 function prof -d 'Profile a program using perf'
     perf record -g --call-graph dwarf -F 10000 $argv \
-        && perf script -F +pid >/tmp/perf.firefox.data
+        && perf script -F +pid >/tmp/perf.firefox.data \
+        && rm perf.data
 end
