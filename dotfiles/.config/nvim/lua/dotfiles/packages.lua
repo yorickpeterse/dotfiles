7ba848a308586d5d35268c130292a68054cfbc87
Yorick Peterse <yorick@yorickpeterse.com>
2025-05-21 18:00:42 +0200

    Switch back to official nvim-treesitter
    Neovim 0.11 introduces a bunch of performance improvements to Tree
    sitter that remove the need for manually disabling various injection
    queries.

--- dotfiles/.config/nvim/lua/dotfiles/packages.lua
+++ dotfiles/.config/nvim/lua/dotfiles/packages.lua
@@ -3,7 +3,7 @@ local use = pkg.use
 
 use('nvim-lua/plenary.nvim')
 use('neovim/nvim-lspconfig')
-use({ 'yorickpeterse/nvim-treesitter', run = 'TSUpdateSync' })
+use({ 'nvim-treesitter/nvim-treesitter', run = 'TSUpdateSync' })
 use('inko-lang/inko.vim')
 use('yorickpeterse/rust.vim')
 use('yorickpeterse/nvim-grey')
