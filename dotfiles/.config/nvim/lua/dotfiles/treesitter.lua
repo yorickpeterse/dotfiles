f288ebe5ab6943195e70d35fc1fa814839a42303
Yorick Peterse <yorick@yorickpeterse.com>
2025-05-23 00:21:30 +0200

    Lazily install Tree-sitter parsers

--- dotfiles/.config/nvim/lua/dotfiles/treesitter.lua
+++ dotfiles/.config/nvim/lua/dotfiles/treesitter.lua
@@ -1,29 +1,62 @@
 local ts = require('nvim-treesitter')
+local uv = vim.uv
+local fn = vim.fn
 
-ts.install({
-  'bash',
-  'c',
-  'css',
-  'fish',
-  'gitcommit',
-  'go',
-  'html',
-  'inko',
-  'javascript',
-  'json',
-  'llvm',
-  'lua',
-  'markdown',
-  'markdown_inline',
-  'python',
-  'query',
-  'ruby',
-  'rust',
-  'toml',
-  'typst',
-  'vimdoc',
-  'yaml',
-})
+-- Per https://github.com/nvim-treesitter/nvim-treesitter/issues/7872 it's
+-- apparently a feature to always log even if there's nothing to be done, so
+-- let's avoid calling install() if there are no changes made to this file. This
+-- way I don't need to remember yet another command/step to run just to set up
+-- an editing environment.
+local install = true
+local time = tostring(uv.fs_stat(debug.getinfo(1, 'S').source:sub(2)).mtime.sec)
+local path = fn.stdpath('cache') .. '/dotfiles_treesitter.txt'
+local file = io.open(path, 'r')
+
+if file then
+  local existing = file:read()
+
+  file:close()
+
+  if existing and existing == time then
+    install = false
+  end
+end
+
+if not file or install then
+  local file = io.open(path, 'w')
+
+  if file then
+    file:write(time)
+    file:close()
+  end
+end
+
+if install then
+  ts.install({
+    'bash',
+    'c',
+    'css',
+    'fish',
+    'gitcommit',
+    'go',
+    'html',
+    'inko',
+    'javascript',
+    'json',
+    'llvm',
+    'lua',
+    'markdown',
+    'markdown_inline',
+    'python',
+    'query',
+    'ruby',
+    'rust',
+    'toml',
+    'typst',
+    'vimdoc',
+    'yaml',
+  })
+end
 
 vim.api.nvim_create_autocmd('FileType', {
   pattern = {
