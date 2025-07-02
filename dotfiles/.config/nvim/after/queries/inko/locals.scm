d261ee8310b9632e2b110cfd524d4ab3374dbdac
Yorick Peterse <yorick@yorickpeterse.com>
2025-07-02 16:13:34 +0200

    Update Inko queries
    The way Snacks groups symbols is rather busted, and while these queries
    result in the right grouping it also results in duplicate symbols for
    the types, which is annoying.

--- dotfiles/.config/nvim/after/queries/inko/locals.scm
+++ dotfiles/.config/nvim/after/queries/inko/locals.scm
@@ -4,11 +4,3 @@
 (source_file) @local.scope
 (import) @local.scope
 (define_constant) @local.scope
-
-((reopen_class
-  name: _ @local.definition.type) @local.scope
- (#set! definition.type.scope "parent"))
-
-((implement_trait
-  class: _ @local.definition.type) @local.scope
- (#set! definition.type.scope "parent"))
