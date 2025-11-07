e43222772a748d6d63f68eee28baed207d0f600d
Yorick Peterse <yorick@yorickpeterse.com>
2025-11-07 13:10:53 +0100

    Place flash label on first character

--- dotfiles/.config/nvim/lua/dotfiles/flash.lua
+++ dotfiles/.config/nvim/lua/dotfiles/flash.lua
@@ -1,18 +1,18 @@
-require('flash').setup({
-  labels = 'tnseriaogmplfuwyqbjdhvkzxc',
-  modes = {
-    search = { enabled = false },
-    char = { enabled = false },
-    treesitter = {
-      label = { before = true, after = false, style = 'overlay' },
-    },
-  },
-  prompt = {
-    enabled = false,
-    win_config = { border = 'none' },
-  },
-  label = {
-    before = true,
-    after = false,
-  },
+require("flash").setup({
+	labels = "tnseriaogmplfuwyqbjdhvkzxc",
+	modes = {
+		search = { enabled = false },
+		char = { enabled = false },
+		treesitter = {
+			label = { before = { 0, 0 }, after = false, style = "overlay" },
+		},
+	},
+	prompt = {
+		enabled = false,
+		win_config = { border = "none" },
+	},
+	label = {
+		before = { 0, 0 },
+		after = false,
+	},
 })
